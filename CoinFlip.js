56704c1bb08833c0f698de1ad22c6ba330d1ebb7
johnny339 <22240170+johnny339@users.noreply.github.com>
2023-04-29 20:44:02 -0600

    Create CoinFlip.js

---
+++ CoinFlip.js
@@ -0,0 +1,44 @@
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".coin");
+let flipBtn = document.querySelector("#flip-button");
+let resetBtn = document.querySelector("#reset-button");
+
+flipBtn.addEventListener("click", () => {
+    let even = 0;
+    let randInt = Math.round(Math.random()*100);
+    console.log(randInt)
+    let i = randInt
+    coin.style.animation = "none";
+    if(i%2 == even){
+        setTimeout(function(){
+            coin.style.animation = "spin-heads 3s forwards";
+        }, 100);
+        heads++;
+    }
+    else{
+        setTimeout(function(){
+            coin.style.animation = "spin-tails 3s forwards";
+        }, 100);
+        tails++;
+    }
+    setTimeout(updateStats, 3000);
+    disableButton();
+});
+function updateStats(){
+    document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
+    document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
+}
+// add a condiditon that if rest button is clicked to enable flip button!
+function disableButton(){
+    flipBtn.disabled = true;
+    setTimeout(function(){
+        flipBtn.disabled = true;
+    });
+}
+resetBtn.addEventListener("click",() => {
+    coin.style.animation = "none";
+    heads = 0;
+    tails = 0;
+    updateStats();
+});
